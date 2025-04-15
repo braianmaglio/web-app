deb63797a28d3d085f0dafa724fb4b60b6eb8968
braianmaglio <braianmaglio76@gmail.com>
2025-04-15 15:38:36 -0300

    cambiando nombre de tabla

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://gwhnhettllgurfhvsjrp.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'Fr@nchu1304'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
